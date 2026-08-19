2ce408e3c26b680bedae2ca64b7a8b9e655aefed
valerie-design297 <valeriebykim@gmail.com>
2026-08-18 21:01:16 -0600

    Update map.js

--- map.js
+++ map.js
@@ -1,13 +1,43 @@
-const map = L.map("map").setView(
-  [39.55, -106.15],
-  9
-);
+// Create the map
+const map = L.map("map").setView([39.55, -106.15], 9);
 
 // Topographic basemap
 L.tileLayer(
-  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
   {
-    maxZoom: 19,
-    attribution: "&copy; OpenStreetMap contributors"
+    maxZoom: 17,
+    attribution:
+      'Map data: &copy; OpenStreetMap contributors, SRTM | Map style: &copy; OpenTopoMap'
   }
 ).addTo(map);
+
+// USGS Watershed Boundary Dataset
+// HUC8 14010002 = Blue River subbasin
+const hucURL =
+  "https://hydro.nationalmap.gov/arcgis/rest/services/wbd/MapServer/4/query" +
+  "?where=huc8%3D%2714010002%27" +
+  "&outFields=*" +
+  "&returnGeometry=true" +
+  "&f=geojson";
+
+fetch(hucURL)
+  .then(response => response.json())
+  .then(data => {
+
+    const watershed = L.geoJSON(data, {
+      style: {
+        color: "#f28c28",
+        weight: 4,
+        opacity: 1,
+        fillColor: "#f28c28",
+        fillOpacity: 0.05
+      }
+    }).addTo(map);
+
+    // Automatically zoom to the Blue River watershed
+    map.fitBounds(watershed.getBounds());
+
+  })
+  .catch(error => {
+    console.error("Error loading watershed boundary:", error);
+  });
